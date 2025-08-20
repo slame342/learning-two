ed23b99434bae7553d5d03208de0aac77d404074
slame342 <105666656+slame342@users.noreply.github.com>
2025-08-20 23:26:26 +0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,48 @@
+# # код до функции
+# def say_hi():
+#     print('я купил кота' + '1')
+#     # код после функции
+#
+#
+# # say_hi()
+# # say_hi()
+# # say_hi()
+# # say_hi()
+#
+# index = 0
+# while index < 4:
+#     say_hi()
+#     index += 1
+
+
+def say_hi(number):
+    print('я купил кота ' + str(number))
+
+
+say_hi(12)
+
+
+# код до функции
+def say_hi():
+    print('я купил кота' + '1')
+
+
+#     # код после функции
+#
+#
+# # say_hi()
+# # say_hi()
+# # say_hi()
+# # say_hi()
+#
+# index = 0
+# while index < 4:
+#     say_hi()
+#     index += 1
+
+
+def say_hi(number):
+    print('я купил кота ' + str(number))
+
+
+say_hi(12)
