a4f783d64cce3dcba58e91ca75fe3de70f9e1c60
slame342 <105666656+slame342@users.noreply.github.com>
2025-08-23 12:08:18 +0500

    up

--- calc.py
+++ calc.py
@@ -14,3 +14,48 @@ print(result)
 
 result = calc(operation="/", number2=3, number1=10)  # именованные аргументы
 print(result)
+
+
+def calc(number1, number2, operations):
+    if operations == "+":
+        return number1 + number2
+    if operations == "-":
+        return number1 - number2
+    if operations == "*":
+        return number1 * number2
+    if operations == "**":
+        return number1 ** number2
+    if operations == "/":
+        if number2 != 0:
+            return number1 / number2
+        else:
+            print('Error!')
+            return print("на нуль делить нельзя!!!")
+    if operations == "//":
+        if number2 != 0:
+            return number1 // number2
+        else:
+            print('Error!')
+            return 0
+    if operations == "%":
+        if number2 != 0:
+            return number1 % number2
+        else:
+            print('Error! вы ввели нуль')
+            return 0
+
+
+result = calc(5, 10, "+")
+print(result)
+result = calc(9, 6, "-")
+print(result)
+result = calc(2, 2, "*")
+print(result)
+result = calc(3, 2, "**")
+print(result)
+result = calc(33, 0, "/")
+print(result)
+result = calc(33, 2, "//")
+print(result)
+result = calc(33, 0, "%")
+print(result)
